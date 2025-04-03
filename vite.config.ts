1649441a4b2a8f6b0f993a5615613d319c293055
nipunrajk <nipunraj99@gmail.com>
2025-04-03 10:06:43 +0530

    checking

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio-web",
+  base: "/",
   resolve: {
     alias: {
       "@components": "/src/components",
